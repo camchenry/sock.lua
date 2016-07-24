d7e49320a4be197811f5426c1e9b654e8ce242e1
Cameron McHenry <camchenry@users.noreply.github.com>
2016-07-24 03:10:19 -0400

    Added hello world example

---
+++ examples/hello/main.lua
@@ -0,0 +1,34 @@
+-- Loading sock from root directory
+package.path = package.path .. ";../../?.lua"
+local sock = require "sock"
+
+function love.load()
+    client = sock.newClient("localhost", 22122)
+    server = sock.newServer("localhost", 22122)
+    
+    -- If the connect/disconnect callbacks aren't defined some warnings will
+    -- be thrown, but nothing bad will happen.
+    
+    -- Called when someone connects to the server
+    server:on("connect", function(data, peer)
+        local msg = "Hello from server!"
+        peer:emit("hello", msg)
+    end)
+    
+    -- Called when a connection is made to the server
+    client:on("connect", function(data)
+        print("Client connected to the server.")
+    end)
+    
+    -- Custom callback, called whenever you send the event from the server
+    client:on("hello", function(msg)
+        print(msg)
+    end)
+
+    client:connect()
+end
+
+function love.update(dt)
+    server:update()
+    client:update()
+end
